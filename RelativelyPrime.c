bb98d8d28bd20f69488f9fca78120f015fc026fc
Aacash Srinath Nambiar <100955640+Aacash-Srinath@users.noreply.github.com>
2022-03-11 16:43:59 +0530

    Update RelativelyPrime.c

--- RelativelyPrime.c
+++ RelativelyPrime.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int i, num1, num2, hcf;
+    int i, num1, num2, hcf, greater;
 
     printf("Enter First Number :: ");
     scanf("%d", &num1);
@@ -10,7 +10,16 @@ int main()
     printf("\nEnter Second Number :: ");
     scanf("%d", &num2);
 
-    for (i=1; i<num1; i++)
+    if (num1>num2)
+    {
+        greater = num1;
+    }
+    else
+    {
+        greater = num2;
+    }
+
+    for (i=1; i<greater; i++)
     {
         if (num1%i == 0 && num2%i == 0)
         {
