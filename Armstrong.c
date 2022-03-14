a8dfb2436aee8d4ae46bc8b81177c9c6713f5e81
Aacash Srinath Nambiar <100955640+Aacash-Srinath@users.noreply.github.com>
2022-03-15 00:05:15 +0530

    Update Armstrong.c

--- Armstrong.c
+++ Armstrong.c
@@ -4,7 +4,7 @@ int main()
 {
     int num, num1, remainder, armstrong = 0; // Declaring Variables for Number, Remainder, & Armstrong
     
-    printf("Enter a three-digit integer: "); //Input Prompt for Number to Check Armstrong Or Not
+    printf("Enter A Three-Digit Integer: "); //Input Prompt for Number to Check Armstrong Or Not
     scanf("%d", &num); // Input number
     
     num1 = num;
@@ -20,12 +20,12 @@ int main()
     //Checking If Input Number is Armstrong or Not
     if (armstrong == num)
     {
-        printf("%d is an Armstrong number.", num);
+        printf("%d Is An Armstrong Number.", num);
     }
 
     else
     {
-        printf("%d is not an Armstrong number.", num);
+        printf("%d Is Not An Armstrong Number.", num);
     }
 
     return 0;
